220535402062e8a7fd419552104c2b43deeb22c3
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-22 06:07:38 +0100

    Create integrate.pyx

---
+++ module/integrate.pyx
@@ -0,0 +1,24 @@
+def f(x):
+    return x ** 2 - x
+
+
+def integrate_f(a, b, N):
+    s = 0
+    dx = (b - a) / N
+    for i in range(N):
+        s += f(a + i * dx)
+    return s * dx
+
+def f(double x):
+    return x ** 2 - x
+
+
+def integrate_f(double a, double b, int N):
+    cdef int i
+    cdef double s
+    cdef double dx
+    s = 0
+    dx = (b - a) / N
+    for i in range(N):
+        s += f(a + i * dx)
+    return s * dx
